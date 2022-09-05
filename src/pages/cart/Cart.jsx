d40d6373c3180f9148b6103c8cca39b77d47d588
Harrison Akunne <harrisonderick.65@gmail.com>
2022-09-05 16:10:41 -0700

    feat: cart summary done

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,12 +1,43 @@
 import React from 'react';
 import './Cart.css';
 import NavBar from '../../components/navBar/NavBar';
+import Button from '../../components/button/Button';
 
 function Cart() {
   return (
     <div className='cart'>
        <NavBar /> 
         <h1 className='cart-header'>Cart</h1>
+
+        <div className='cart-body'>
+            <h3>Your Cart</h3>
+            <div className='cart-body-inner'>
+                <div className='cart-summary'>
+                    <p className='cart-summary-item cart-summary-item-header'>
+                        <p>cart summary</p>
+                    </p>
+                    <p className='cart-summary-item'>
+                        <p>subtotal</p>
+                        <p>$0.00</p>
+                    </p>
+                    <p className='cart-summary-item'>
+                        <p>shipping price</p>
+                        <p>$0.00</p>
+                    </p>
+                    <p className='cart-summary-item'>
+                        <p>grand total</p>
+                        <p>$0.00</p>
+                    </p>
+
+                    <p className='cart-summary-disclaimer'>
+                        <p className='disclaimer'>Shipping price might change based on your location</p>
+                        <Button content="CHECK OUT" style={{borderRadius: 5}} className='button' />
+                    </p>
+                    
+                </div>
+            </div>
+        </div>
+        
     </div>
   )
 }
