b36827cd63c4a649af03a65c83bd1626c5f9bc0d
Harrison Akunne <harrisonderick.65@gmail.com>
2022-09-11 05:56:43 -0700

    chore: conflicts resolved

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -3,6 +3,7 @@ import './Cart.css';
 import NavBar from '../../components/navBar/NavBar';
 import Button from '../../components/button/Button';
 import { BsArrowLeft } from 'react-icons/bs';
+import cart from "./cart.json";
 function Cart() {
   return (
     <div className='cart'>
@@ -12,6 +13,34 @@ function Cart() {
         <div className='cart-body'>
             <h3>Your Cart</h3>
             <div className='cart-body-inner'>
+
+                <div className='items-div'>
+                    
+                    <p className='items-div-header'>Product</p>
+                    <p className='items-div-header'>Price</p>
+                    <p className='items-div-header'>Quantity</p>
+                    <p className='items-div-header'>Total</p>
+
+                    {
+                        cart.map((item) => (
+                            <React.Fragment key={item._id}>
+                                <p className='item'>
+                                    <img src={require(`../../assets/${item.img_url}`)} alt="cart-item" className='item-img'/>
+                                    <p className='item-name'>{item.name}</p>
+                                </p>
+                                <p className='item'>{item.price}</p>
+                                <p className='item'>
+                                    <Button content="+" style={{width: "40px", height: "40px"}} />
+                                    <p className='item-quantity'>{12}</p>
+                                    <Button content="-" style={{width: "40px", height: "40px"}} />
+                                </p>
+                                <p className='item'>{2400}</p>
+                            </React.Fragment>
+                        ))
+                    }
+
+                </div>
+
                 <div className='cart-summary'>
                     <p className='cart-summary-item cart-summary-item-header'>
                         <p>cart summary</p>
