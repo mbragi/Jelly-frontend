2edd6787b8c6d68ebb36ccba53e7b5b46955e0c8
Achilihu Daniel Uchenna <achilihudaniel53@gmail.com>
2022-09-16 03:14:09 +0100

    creating CustomFooter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React from "react";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { path } from "./routes";
-// import Home from "./pages/home/Home";
-// import LoginPage from "./pages/login/LoginPage";
-// import Cart from "./pages/cart/Cart.jsx";
-// import RegisterPage from "./pages/register/RegisterPage";
-// import CheckOut from "./pages/checkout/CheckOut";
-// import ContactPage from './pages/contact/ContactPage'
-import Footer from './components/footer/Footer'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { path } from "./routes";
+import Home from "./pages/home/Home";
+import LoginPage from "./pages/login/LoginPage";
+import Cart from "./pages/cart/Cart.jsx";
+import RegisterPage from "./pages/register/RegisterPage";
+import CheckOut from "./pages/checkout/CheckOut";
+import ContactPage from './pages/contact/ContactPage'
 function App() {
   return (
     <>
-      {/* <section style={{ width: "100%" }}> */}
-        {/* <BrowserRouter>
-          <Routes> */}
+      <section style={{ width: "100%" }}>
+        <BrowserRouter>
+          <Routes>
             {/* <Route exact path="/" element={<Home />} />
             <Route exact path="/login" element={<LoginPage />} />
             <Route exact path="/register" element={<RegisterPage />} />
@@ -21,7 +20,7 @@ function App() {
             <Route exact path="/cart" element={<Cart />} />
 <<<<<<< HEAD
             <Route exact path="/checkout" element={<CheckOut />} /> */}
-{/* 
+
             <Route exact path={path.homepage} element={<Home />} />
             <Route exact path={path.loginPage} element={<LoginPage />} />
             <Route exact path={path.registerpage} element={<RegisterPage />} />
@@ -30,9 +29,8 @@ function App() {
             <Route exact path="/contact" element={<ContactPage />} />
             <Route exact path="*" element={<h1>404</h1>} />
           </Routes>
-        </BrowserRouter> */}
-      {/* </section> */}
-      <Footer/>
+        </BrowserRouter>
+      </section>
     </>
   );
 }
