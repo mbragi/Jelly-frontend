140bebe06a1dc655d11465d13bdafa6bd4bdacea
Harrison Akunne <harrisonderick.65@gmail.com>
2022-09-17 17:43:50 +0100

    chore: changed pagination content

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -12,7 +12,7 @@ function Products() {
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalProducts, setTotalProducts] = useState(0);
-  const productsPerPage = 6;
+  const productsPerPage = 8;
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
