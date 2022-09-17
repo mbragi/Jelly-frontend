bb792694bc19e66ebd847beb30307d195264e9a7
Harrison Akunne <harrisonderick.65@gmail.com>
2022-09-17 17:18:02 +0100

    chore: removed hamburger icon

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -12,7 +12,7 @@ function Products() {
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalProducts, setTotalProducts] = useState(0);
-  const productsPerPage = 2;
+  const productsPerPage = 6;
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
