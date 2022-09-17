a5e4ab1f4eba9c90e5928a520486a654b8a63f3b
Harrison Akunne <harrisonderick.65@gmail.com>
2022-09-17 16:45:50 +0100

    chore: changed some PNGs to JPGs

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,8 @@ import React, { useState } from 'react';
 import NavBar from "../../components/navBar/NavBar";
 import intro from "../../assets/intro.mp4";
 import features from "../../assets/features.png";
-import bike from "../../assets/bike.png";
-import turnSignal from "../../assets/turn-signal.png";
+import bike from "../../assets/bike.jpg";
+import turnSignal from "../../assets/turn-signal.jpg";
 import Button from "../../components/button/Button";
 import Footer from '../../components/footer/Footer';
 import "./Home.css";
@@ -12,7 +12,7 @@ import { MdDirectionsBike, MdDirectionsCar, MdDirectionsBus, MdOutlineStar } fro
 import { Fade, Zoom } from "react-awesome-reveal";
 function Home() {
   const [featuresIndex, setFeaturesIndex] = useState(0);
-  const featuresArray = ["bike.png", "turn-signal.png"]
+  const featuresArray = ["bike.jpg", "turn-signal.jpg"]
   const prev = () => {
     setFeaturesIndex(featuresIndex => {
       if (featuresIndex === 0) return featuresArray.length - 1;
