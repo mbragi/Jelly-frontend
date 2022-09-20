458541b8ae8f3bf0ff3749a21f51b5f604b815a0
unknown <spontaneousmanji@gmail.com>
2022-09-20 23:02:14 +0100

    bug : pagination and mapping categories

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -4,10 +4,10 @@ import Button from '../button/Button';
 function Product({ product }) {
   return (
     <div className='product'>
-        <p className='product-name'>{product.name}</p>
-        <img src={require(`../../assets/${product.img}`)} alt={product.name} className='product-img' /> 
-        <p className="product-price">${product.price}</p>
-        <Button content="Details" style={{borderRadius: "10px"}} />
+      <p className='product-name'>{product.name}</p>
+      <img src={product.img} alt={product.name} className='product-img' />
+      <p className="product-price">${product.price}</p>
+      <Button content="Details" style={{ borderRadius: "10px" }} />
     </div>
   )
 }
--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,24 +1,34 @@
-import React, {useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../../components/navBar/NavBar';
 import Button from '../../components/button/Button';
 import './Products.css';
 import logoFaint from '../../assets/logo-faint.png';
 import Product from '../../components/product/Product';
 import Paigination from '../../components/pagination/Pagination';
-import JSONproducts from './products.json';
 import Footer from '../../components/footer/Footer';
 
 function Products() {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([])
   const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalProducts, setTotalProducts] = useState(0);
+
   const productsPerPage = 8;
   useEffect(() => {
+    const URL = process.env.REACT_APP_SERVER_URL
     const fetchData = async () => {
       setLoading(true);
-      setProducts(JSONproducts);
-      setTotalProducts(JSONproducts.length);
+      const res = await fetch(`${URL}/category`)
+      const data = await res.json()
+      const category = [data.Cdata]
+      const product = data.Pdata
+      setCategories(category)
+      // console.log(categories)
+      setProducts(product);
+      // console.log(products)
+
+      setTotalProducts(product.length);
       setLoading(false);
     };
     fetchData();
@@ -27,85 +37,95 @@ function Products() {
   const indexOfLastProduct = currentPage * productsPerPage;
   const indexOfFirstProduct = indexOfLastProduct - productsPerPage;
   const currentProducts = products.slice(indexOfFirstProduct, indexOfLastProduct);
-  // console.log(currentProducts.length);
-  
+  console.log(currentProducts.length);
 
   return (
     <div className='container'>
-        <NavBar currentPage="shop" />
-        <div className='products-page'>
+      <NavBar currentPage="shop" />
+      <div className='products-page'>
 
-          <div className='product-header'>
-            <img src={logoFaint} alt="logo" className='header-logo' />
-          </div>
+        <div className='product-header'>
+          <img src={logoFaint} alt="logo" className='header-logo' />
+        </div>
 
-          <div className='sort-container'>
-            <select className='select'>
-              <option value="">sort</option>
-              <option value="100">100</option>
-              <option value="200">200</option>
-            </select>
-          </div>
+        <div className='sort-container'>
+          <select className='select'>
+            <option value="">sort</option>
+            <option value="100">100</option>
+            <option value="200">200</option>
+          </select>
+        </div>
 
-          <div className='product-body'>
-            <div className='categories'>
-              <h2>Categories</h2>
+        <div className='product-body'>
+          <div className='categories'>
+            <h2>Categories</h2>
 
-              <div className='category'>
-                <input id="" type="checkbox" name="" />
-                <label htmlFor='' className='label'>Li-ion cell for EV</label>
-              </div>
-              
-              <div className='category'>
-                <input id="" type="checkbox" name="" />
-                <label htmlFor='' className='label'>LFP BMS</label>
-              </div>
+            {/* {loading ? <h1>Loading...</h1> : */}
+            {/* categories.map((category, idx) => { */}
+            {console.log(categories)}
+            {/*  return ( */}
+            {/* <div className='category' key={idx}> */}
+            {/* <input id={`${category._id}`} type="checkbox" name={category.categoryName} /> */}
+            {/* <label htmlFor='' className='label'>{category.categoryName}</label> */}
+            {/* </div> */}
+            {/* ) */}
+            {/*  }) */}
+            {/* } */}
 
-              <div className='category'>
-                <input id="" type="checkbox" name="" />
-                <label htmlFor='' className='label'>Li-ion cell for EV</label>
-              </div>
-              
-              <div className='category'>
-                <input id="" type="checkbox" name="" />
-                <label htmlFor='' className='label'>LFP BMS</label>
-              </div>
-
-              <div className='category'>
-                <input id="" type="checkbox" name="" />
-                <label htmlFor='' className='label'>Li-ion cell for EV</label>
-              </div>
-              
-              <div className='category'>
-                <input id="" type="checkbox" name="" />
-                <label htmlFor='' className='label'>LFP BMS</label>
-              </div>
-
-              <div className="price-filter">
-                <h2>Price Filter</h2>
-                <input type="range" className='price-range' />
-                <div className='price'>
-                  <p><span>Price</span>: $0.00 - $0.00</p>
-                  <Button content="Filter" style={{borderRadius: "10px"}} />
-                </div>
-              </div>
-
+            <div className='category'>
+              <input id="" type="checkbox" name="" />
+              <label htmlFor='' className='label'>LFP BMS</label>
             </div>
+
+            <div className='category'>
+              <input id="" type="checkbox" name="" />
+              <label htmlFor='' className='label'>Li-ion cell for EV</label>
+            </div>
+
+            <div className='category'>
+              <input id="" type="checkbox" name="" />
+              <label htmlFor='' className='label'>LFP BMS</label>
+            </div>
+
+            <div className='category'>
+              <input id="" type="checkbox" name="" />
+              <label htmlFor='' className='label'>Li-ion cell for EV</label>
+            </div>
+
+            <div className='category'>
+              <input id="" type="checkbox" name="" />
+              <label htmlFor='' className='label'>LFP BMS</label>
+            </div>
+
+            <div className="price-filter">
+              <h2>Price Filter</h2>
+              <input type="range" className='price-range' />
+              <div className='price'>
+                <p><span>Price</span>: $0.00 - $0.00</p>
+                <Button content="Filter" style={{ borderRadius: "10px" }} />
+              </div>
+            </div>
+
+          </div>
 
           <div className='products'>
 
-            {loading ? <hi>Loading...</hi> : 
-              currentProducts.map((product) => (
-                <Product key={product._id} product={product} />
-              ))
+            {loading ? <h1>Loading...</h1> :
+              products.map((product, index) => {
+                return (
+                  < Product key={index} product={product} />
+                  // console.log(`${product}`)
+                )
+              })
+
             }
           </div>
-          </div>
-            <Paigination productsPerPage={productsPerPage} totalProducts={totalProducts} currentPage={currentPage} setCurrentPage={setCurrentPage} />
-
         </div>
-        <Footer />
-    </div>
+        <Paigination productsPerPage={productsPerPage} totalProducts={totalProducts} currentPage={currentPage} setCurrentPage={setCurrentPage} />
+
+      </div >
+      <Footer />
+    </div >
   )
 }
 
