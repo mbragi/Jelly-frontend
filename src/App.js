90d38414ffcd6d867287c817c53799b30c156969
Achilihu Daniel Uchenna <achilihudaniel53@gmail.com>
2022-09-22 15:16:34 +0100

    chore:setting the state

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import RegisterPage from "./pages/register/RegisterPage";
 import CheckOut from "./pages/checkout/CheckOut";
 import ContactPage from './pages/contact/ContactPage'
 import Products from './pages/products/Products.jsx'
+import ForgotPassword from './pages/forgotPassword/ForgotPassword'
+
 function App() {
   return (
     <>
@@ -25,6 +27,7 @@ function App() {
 
             <Route exact path="/contact" element={<ContactPage />} />
             <Route exact path="/details" element={<ProductDetails/>} />
+            <Route exact path="/forgotpassword" element={<ForgotPassword/>} />
             <Route exact path="*" element={<h1>404</h1>} />
           </Routes>
         </BrowserRouter>
