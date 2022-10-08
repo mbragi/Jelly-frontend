a018db86ae1013e9cc5f3ec9166b3161fe430663
Fiki-josh <fikioluwamayomikun@gmail.com>
2022-10-08 21:48:31 +0100

    chore: reupload of adding mobile navigation

--- src/components/mobileBar/MobileBar.jsx
+++ src/components/mobileBar/MobileBar.jsx
@@ -2,21 +2,29 @@ import React from 'react'
 import './MobileBar.css';
 import { HiHome, HiShoppingCart } from "react-icons/hi";
 import { BsShopWindow } from "react-icons/bs";
+import {Link} from "react-router-dom";
 function MobileBar(){
+    const cart = localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')).length : 0;
     return(
         <div className='mobile-bar'>
-            <div className="mobile-icons">
-                <HiHome size ={30} />
-                <h3>Home</h3>
-            </div>
-            <div className="mobile-icons">
-                <BsShopWindow size ={30} />
-                <h3>Shop</h3>
-            </div>
-            <div className="mobile-icons">
-                <HiShoppingCart size ={30} />
-                <h3>Cart(0)</h3>
-            </div>
+            <Link to = '/' className='mobile-link'>
+                <div className="mobile-icons">
+                    <HiHome size ={30} />
+                    <h3>Home</h3>
+                </div>
+            </Link>
+            <Link to = '/shop' className='mobile-link'>
+                <div className="mobile-icons">
+                    <BsShopWindow size ={30} />
+                    <h3>Shop</h3>
+                </div>
+            </Link>
+            <Link to = '/cart' className='mobile-link'>
+                <div className="mobile-icons">
+                    <HiShoppingCart size ={30} />
+                    <h3>Cart({cart})</h3>
+                </div>
+            </Link>
         </div>
     )
 }
