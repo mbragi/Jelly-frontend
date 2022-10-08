c10d73026331484d7e0d1805e479814b369b0206
Fiki-josh <fikioluwamayomikun@gmail.com>
2022-10-08 20:48:34 +0100

    chore: adding mobile navigation

---
+++ src/components/mobileBar/MobileBar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './MobileBar.css';
+import { HiHome, HiShoppingCart } from "react-icons/hi";
+import { BsShopWindow } from "react-icons/bs";
+function MobileBar(){
+    return(
+        <div className='mobile-bar'>
+            <div className="mobile-icons">
+                <HiHome size ={33} />
+                <h3>Home</h3>
+            </div>
+            <div className="mobile-icons">
+                <BsShopWindow size ={33} />
+                <h3>Shop</h3>
+            </div>
+            <div className="mobile-icons">
+                <HiShoppingCart size ={33} />
+                <h3>Cart(0)</h3>
+            </div>
+        </div>
+    )
+}
+
+export default MobileBar
