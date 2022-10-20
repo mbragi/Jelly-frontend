a59c2971680c788883d1c83c115bfee9e54a3ade
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-10-20 07:18:34 +0100

    chore: created the admin users file

---
+++ src/pages/adminUsers/AdminUsers.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import AdminFrame from '../../components/adminFrame/AdminFrame'
+function AdminUsers() {
+  return (
+    <AdminFrame currentPage={"users"}>
+        
+    </AdminFrame>
+  )
+}
+
+export default AdminUsers
