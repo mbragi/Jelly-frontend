381ffe454215070576d4d87b1e5b76ba1be746d0
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-10-20 07:32:58 +0100

    chore: created the jsx element

--- src/pages/adminUsers/AdminUsers.jsx
+++ src/pages/adminUsers/AdminUsers.jsx
@@ -5,7 +5,19 @@ import './AdminUsers.css'
 function AdminUsers() {
   return (
     <AdminFrame currentPage={"users"}>
-        
+        <div className="admin-users-container">
+            <header>
+                <Button content={'Add Admin'} style ={{backgroundColor: ' rgba(53, 112, 236, 0.67)'}} />
+            </header>
+            <section>
+                <header className='user-lists-header'>
+                    <p>User List</p>
+                </header>
+                <div>
+
+                </div>
+            </section>
+        </div>
     </AdminFrame>
   )
 }
