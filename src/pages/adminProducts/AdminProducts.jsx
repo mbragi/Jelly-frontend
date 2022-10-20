562487bd7b60aed0028fcfd633c945e52039657c
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-10-20 17:51:05 +0100

    chore: added the adminProduct codes

--- src/pages/adminProducts/AdminProducts.jsx
+++ src/pages/adminProducts/AdminProducts.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
 import './AdminProducts.css'
+import AdminFrame from '../../components/adminFrame/AdminFrame'
+import Button from '../../components/button/Button'
 function AdminProducts() {
   return (
-    <div>AdminProducts</div>
+    <AdminFrame currentPage={'products'}>
+        <div className='admin-products-container'>
+           <header className='admin-product-add-header'>
+                <h1>Products</h1>
+                <Button content={'Add Products'} style ={{
+                    backgroundColor: ' rgba(53, 112, 236, 0.67)',
+                    borderRadius: '8px',
+                    width:'150px'
+                }} />
+           </header>
+        </div>
+    </AdminFrame>
   )
 }
 
