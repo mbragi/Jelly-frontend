85fbfdefd9b08bb13a4b5b47a3fe3559f82b6482
unknown <spontaneousmanji@gmail.com>
2022-10-27 15:41:14 +0100

    chore: modifying code

--- src/pages/adminAddProduct/AdminAddProduct.jsx
+++ src/pages/adminAddProduct/AdminAddProduct.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './AdminAddProduct.css'
-import  addproducts from './addproducts.json'
+import addproducts from './addproducts.json'
 import AdminFrame from '../../components/adminFrame/AdminFrame';
 import Button from '../../components/button/Button'
 import pen from '../../assets/images/pen.png'
@@ -25,17 +25,17 @@ function AdminAddProduct() {
                     <div className='admin-add-product-images'>
                         <div className='upload-main-image'>
                             <input type="file" id='file' />
-                            <label for= "file">
+                            <label for="file">
                                 <img src={pen} alt={'addproduct'} style={{
                                     width: '50px',
                                     height: '40px',
-                                    float:'right',
-                                    borderRadius:'5px',
+                                    float: 'right',
+                                    borderRadius: '5px',
                                     backgroundColor: '#FFFFFF',
                                     boxShadow: '0px 0.8718905448913574px 15.694029808044434px 0px #00000040',
                                     objectFit: 'contain',
                                     padding: '5px'
-                                }}  />
+                                }} />
                             </label>
                             <h1 className='umi'>Upload Main Image</h1>
                         </div>
@@ -44,19 +44,19 @@ function AdminAddProduct() {
                             {addproducts.map((addproduct, index) => (
                                 <React.Fragment key={index}>
                                     <div className='add-sub-product-image'>
-                                    <input type="file" id='file' accept='image'/>
-                                    <label for= "file">
-                                        <img src={pen} alt={addproduct.img}  style={{
-                                        width: '40.0px',
-                                        height: '30.0px',
-                                        float:'right',
-                                        borderRadius:'5px',
-                                        backgroundColor: '#FFFFFF',
-                                        boxShadow: '0px 0.8718905448913574px 15.694029808044434px 0px #00000040',
-                                        objectFit: 'contain',
-                                        padding: '5px'
-                                        }} />
-                                    </label>
+                                        <input type="file" id='file' accept='image' onChange={(e) => { console.log(e.target.files) }} />
+                                        <label for="file">
+                                            <img src={pen} alt={addproduct.img} style={{
+                                                width: '40.0px',
+                                                height: '30.0px',
+                                                float: 'right',
+                                                borderRadius: '5px',
+                                                backgroundColor: '#FFFFFF',
+                                                boxShadow: '0px 0.8718905448913574px 15.694029808044434px 0px #00000040',
+                                                objectFit: 'contain',
+                                                padding: '5px'
+                                            }} />
+                                        </label>
                                         <h5 className='usi'>Upload Sub Image</h5>
                                     </div>
 
@@ -74,7 +74,7 @@ function AdminAddProduct() {
                                     <div className='addproduct-name'>
                                         <h1>PRODUCT NAME</h1>
                                         <input type="text" className='pn-sc-inpt' />
-                                        
+
                                     </div>
 
                                     <div className='select-categories'>
@@ -118,7 +118,7 @@ function AdminAddProduct() {
                             <div className='dif-colours'>
                                 <div className='diff-colours'>
                                     <h1>COLOURS
-                                        <span className='add-colours'> Add <Button type={'submit'} content={'+'} style={{ backgroundColor: 'blue', width: '35px', height: '35px', fontSize:'20px' }} />
+                                        <span className='add-colours'> Add <Button type={'submit'} content={'+'} style={{ backgroundColor: 'blue', width: '35px', height: '35px', fontSize: '20px' }} />
                                         </span>
                                     </h1>
 
