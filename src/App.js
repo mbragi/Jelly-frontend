53786b1c4a27a88f2fbbdfb64d8b468e98082643
Oladimeji.Dev <salimanoladimeji33@gmail.com>
2022-11-04 15:52:01 +0100

    feat: Private Route features added to the admin pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { path } from "./routes";
 import React from "react";
+import PrivateRoutes from "./utils/PrivateRoutes";
 import ProductDetails from "./pages/productDetails/ProductDetails";
 import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart.jsx";
@@ -13,11 +14,8 @@ import VideoSection from "./pages/videoSection/VideoSection";
 import Ncm from "./pages/specification/ncm/Ncm";
 import Lfp from "./pages/specification/lfp/Lfp";
 import UploadImage from "./utils/cloudinary";
-
 import AdminDashboard from './pages/adminDashboard/AdminDashboard';
-
 import AdminCategory from "./pages/adminCategory/AdminCategory";
-
 import AdminUsers from "./pages/adminUsers/AdminUsers";
 import AdminProducts from "./pages/adminProducts/AdminProducts";
 import AdminAddProduct from "./pages/adminAddProduct/AdminAddProduct";
@@ -38,20 +36,34 @@ function App() {
             <Route exact path="/contact" element={<ContactPage />} />
             <Route exact path="/details/:id" element={<ProductDetails />} />
             <Route exact path="/forgotpassword" element={<ForgotPassword />} />
-            <Route exact path="/admin" element={<AdminDashboard />} />
 
-            <Route exact path="/admin/dashboard" element={<AdminDashboard />} />
-              <Route exact path="/admin/categories" element={<AdminCategory />} />
-            <Route exact path="/admin/users" element={<AdminUsers />} />
+            <Route path='/admin' element = {<PrivateRoutes/>}>
+              <Route  path="/admin" element={<AdminDashboard />} />
+            </Route>
+            <Route path='/admin/dashboard' element = {<PrivateRoutes/>}>
+              <Route path="/admin/dashboard" element={<AdminDashboard />} />
+            </Route>
+            <Route path='/admin/categories' element = {<PrivateRoutes/>}>
+            <Route exact path="/admin/categories" element={<AdminCategory />} />
+            </Route>
+            <Route path='/admin/users' element = {<PrivateRoutes/>}>
+              <Route exact path="/admin/users" element={<AdminUsers />} />
+            </Route>
+            <Route path='/admin/products' element = {<PrivateRoutes/>}>
+              <Route exact path="/admin/products" element={<AdminProducts />} />
+            </Route>
+            <Route path='/admin/addproduct' element = {<PrivateRoutes/>}>
+              <Route exact path="/admin/addproduct" element={<AdminAddProduct />} />
+            </Route>
+            
+            
 
-            <Route exact path="/admin/products" element={<AdminProducts />} />
-            <Route exact path="/admin/addproduct" element={<AdminAddProduct />} />
             <Route exact path="/product" element={<SpecHead />} />
             <Route exact path="/video" element={<VideoSection />} />
             <Route exact path="/product/ncm" element={<Ncm />} />
             <Route exact path="/product/lfp" element={<Lfp />} />
             <Route exact path="/upload" element={<UploadImage />} />
-            <Route exact path="*" element={<h1>404</h1>} />
+            <Route exact path="/*" element={<h1>404</h1>} />
           </Routes>
         </HashRouter>
       </section>
