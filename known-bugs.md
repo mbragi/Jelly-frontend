887876173346ac0f62a6be585ee41462c7ff161b
unknown <spontaneousmanji@gmail.com>
2022-11-06 08:37:25 +0100

    fix: preview sub images and image

--- known-bugs.md
+++ known-bugs.md
@@ -1,3 +1,5 @@
 products page: clicking page 2, 3, ..., n before filtering prices causes an empty page
 
 adding or removing from cart does not reflect immediately on th UI (until the page refreshes)
+
+adding images to sub images of products does not preview till you add the next image
--- src/utils/auth/Auth.js
+++ src/utils/auth/Auth.js
@@ -1,29 +1,20 @@
-import { useEffect,useState } from "react"
+import { useEffect, useState } from "react";
 
 export const Auth = () => {
-    const[auth ,setAuth] = useState(false)
-    const[userData ,setUserData] = useState(false)
-    const[loading ,setLoading] = useState(true)
-    
-    useEffect(()=>{
-        const auths = JSON.parse(localStorage.getItem('auth'))
-        auths.hasOwnProperty("token") === true ? setAuth(auths.token) : setAuth(false)
-        const userD = JSON.parse(localStorage.getItem('userData'))
-        userD ? setUserData(userD.isAdmin) : setUserData(false)
-        
-        setLoading(false)
+  const [auth, setAuth] = useState(false);
+  const [userData, setUserData] = useState(false);
+  const [loading, setLoading] = useState(true);
 
-    },[])
+  useEffect(() => {
+    const auths = JSON.parse(localStorage.getItem("auth"));
+    auths.hasOwnProperty("token") === true
+      ? setAuth(auths.token)
+      : setAuth(false);
+    const userD = JSON.parse(localStorage.getItem("userData"));
+    userD ? setUserData(userD.isAdmin) : setUserData(false);
 
+    setLoading(false);
+  }, []);
 
-    
-
-    return{auth,userData,loading}
-
-    
-}
-
-
-
-
-
+  return { auth, userData, loading };
+};
