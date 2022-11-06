f3fc2bd097d6474e497b1fcd0d37be4a22a349f0
Achilihu Daniel Uchenna <achilihudaniel53@gmail.com>
2022-11-06 12:57:23 +0100

    chore: creating sections page

--- src/pages/sectionII/SectionII.jsx
+++ src/pages/sectionII/SectionII.jsx
@@ -81,7 +81,7 @@ function SectionII() {
                         <p> Next</p>
                         <BsArrowRight size={30} />
                     </div>
-                    
+
                 </div>
 
             </AdminFrame>
