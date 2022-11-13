d1c2f9e631219ff33636aad3ece9301a7d6d8675
Fiki-josh <fikioluwamayomikun@gmail.com>
2022-11-13 16:40:55 +0100

    chore: another change2

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './Welcome.css';
-import animate from '../../assets/animated-welcome-gif-8.gif'
-import { MdCancel } from "react-icons/md"; 
+import animate from '../../assets/animated-welcome-gif-8.gif' 
 import {useGlobalContext} from '../../context';
 function Welcome(){
     const {setIsSignUp} = useGlobalContext()
@@ -13,7 +12,7 @@ function Welcome(){
             <div className='welcome-div'>
                 <div className='welcome-sub'>
                     <span>
-                        <span className='welcome-cancel' id= "sign_welcome" onClick={closeWelcome}><MdCancel size={30} /></span>
+                        <span className='welcome-cancel' id= "sign_welcome" onClick={closeWelcome}>	╳</span>
                     </span>
                     <img src={animate} alt="welcome" />
                 </div>
