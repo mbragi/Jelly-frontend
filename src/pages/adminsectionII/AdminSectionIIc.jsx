854d42a5843d3aeee5b4b76ffd135dd25191681e
m@nji <spontaneousmanji@gmail.com>
2022-11-20 09:55:27 +0100

    chore: implemented admin upload image

--- src/pages/adminsectionII/AdminSectionIIc.jsx
+++ src/pages/adminsectionII/AdminSectionIIc.jsx
@@ -2,45 +2,65 @@ import React from 'react'
 import './AdminSectionIIc.css'
 import AdminFrame from '../../components/adminFrame/AdminFrame';
 import pen from '../../assets/images/pen.png'
-import {BsArrowRight } from 'react-icons/bs';
-import { CloudArrowUp, Plus } from 'phosphor-react';
+import { BsArrowRight } from 'react-icons/bs';
+import { CloudArrowUp } from 'phosphor-react';
 import { Link } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import axios from 'axios';
+const BASE_URL = 'https://jelly-online-api.herokuapp.com'
+
 
 
 function SectionIIc() {
+    const [loading, setLoading] = useState(false)
+    const [data, setData] = useState({})
 
-    
-  return (
-         <div>
-           <AdminFrame currentPage='sectionIIc'>
+    async function httpGetBanner() {
+        const res = await axios.get(`${BASE_URL}/api/app/banner`)
+        const response = res.data.data
+        const map = response.map(item => item).reverse()
+        console.log(map)
+        setData(map[0])
+    }
+
+    useEffect(() => {
+        setLoading(true)
+        httpGetBanner()
+        setLoading(false)
+    }, [])
+
+    return (
+        <div>
+            <AdminFrame currentPage='sectionIIc'>
                 <div className='sectionIIc'>
                     <header className='sectionIIc-header'>
                         <h1>Section II</h1>
 
-                        <button type='file' 
-                            style={{ 
-                                cursor: 'pointer', 
-                                background: " rgb(53, 112, 236)", 
-                                height: '3.5rem', 
-                                width: '13rem', 
-                                float: "right", 
+                        <button type='file'
+                            style={{
+                                cursor: 'pointer',
+                                background: " rgb(53, 112, 236)",
+                                height: '3.5rem',
+                                width: '13rem',
+                                float: "right",
                                 border: 'none',
                                 color: 'white',
-                                borderRadius:'15px'
+                                borderRadius: '15px'
                             }}>
 
                             <label id='label'>
-                                <input  type="file" style={{  
-                                    width: '1rem' 
+                                <input type="file" style={{
+                                    width: '1rem'
                                 }} />
 
                                 <span style={{
                                     display: 'flex',
-                                    gap:'5px', 
-                                    alignItems: "center", 
-                                    justifyContent: 'center', 
-                                    width: '100%' 
-                                }}>                           
+                                    gap: '5px',
+                                    alignItems: "center",
+                                    justifyContent: 'center',
+                                    width: '100%'
+                                }}>
                                     <CloudArrowUp size={18} />  Upload
                                 </span>
                             </label>
@@ -49,15 +69,15 @@ function SectionIIc() {
                     </header>
 
                     <section className='sectionIIc-container'>
-                        <div className='sectionIIc-add-image'>
+                        {/* <div className='sectionIIc-add-image'>
                                
                                 <Plus size={20} style= {{cursor:'pointer'}}/>
                                 <p> Add Image</p>
-                        </div>
+                        </div> */}
 
-                        <div className='upload-main-image-sectionIIc'>
+                        <div className='uploaded-main-image-sectionIIc'>
 
-                            <input id='filezz' type="file" name='img' />
+                            {/* <input id='filezz' type="file" name='img' />
                             <label htmlFor="filezz">
                                 <img src={pen} alt="product" style={{
                                     width: '50px',
@@ -69,14 +89,15 @@ function SectionIIc() {
                                     boxShadow: '0px 0.8718905448913574px 15.694029808044434px 0px #00000040',
                                     objectFit: 'contain'
                                 }} />
-                            </label>
+                            </label> */}
                             {
-                                <h1 className='umi-secc'>Upload Main <br /> Image</h1>
+                                loading ?
+                                    <h1 className='umi-secc'>Preview Uploaded Home <br /> Image</h1>
+                                    : <img src={data.photoUrl} alt="sec1" className="umi-sec" />
                             }
                         </div>
 
                     </section>
-
                     <div className='uploaded-image-previewed-here'>
                         <>
                             <div className='uploaded-image-previewed' >
@@ -91,13 +112,13 @@ function SectionIIc() {
                                         objectFit: 'contain',
                                         padding: '5px'
                                     }} />
-                                    <input name="option_one" id='one' type="file"  style={{
+                                    <input name="img_one" id='one' type="file" style={{
                                         opacity: 0, position: 'absolute', width: '40.0px',
                                         height: '30.0px',
                                         float: 'right',
                                         borderRadius: '5px',
                                     }} />  {
-                                        
+
                                         <h5 className='uiph'>Uploaded image is previewed here</h5>
                                     }
                                 </label>
@@ -118,19 +139,19 @@ function SectionIIc() {
                                         objectFit: 'contain',
                                         padding: '5px'
                                     }} />
-                                    <input name="option_two" id='two' type="file" style={{
+                                    <input name="img_two" id='two' type="file" style={{
                                         opacity: 0, position: 'absolute', width: '40.0px',
                                         height: '30.0px',
                                         float: 'right',
                                         borderRadius: '5px',
                                     }} />  {
-                                        
+
                                         <h5 className='uiph'>Uploaded image is previewed here</h5>
                                     }
                                 </label>
                             </div>
-                        </> 
-                        
+                        </>
+
                         <>
                             <div className='uploaded-image-previewed'>
                                 <label htmlFor='three' >
@@ -145,19 +166,19 @@ function SectionIIc() {
                                         objectFit: 'contain',
                                         padding: '5px'
                                     }} />
-                                    <input name="option_three" id='three' type="file"style={{
+                                    <input name="img_three" id='three' type="file" style={{
                                         opacity: 0, position: 'absolute', width: '40.0px',
                                         height: '30.0px',
                                         float: 'right',
                                         borderRadius: '5px',
                                     }} />  {
-                                        
+
                                         <h5 className='uiph'>Uploaded image is previewed here</h5>
                                     }
                                 </label>
                             </div>
-                        </> 
-                        
+                        </>
+
                         <>
                             <div className='uploaded-image-previewed' >
                                 <label htmlFor='four'>
@@ -172,30 +193,29 @@ function SectionIIc() {
                                         objectFit: 'contain',
                                         padding: '5px'
                                     }} />
-                                    <input name="option_four" id='four' type="file"  style={{
+                                    <input name="img_four" id='four' type="file" style={{
                                         opacity: 0, position: 'absolute', width: '40.0px',
                                         height: '30.0px',
                                         float: 'right',
                                         borderRadius: '5px',
                                     }} />  {
-                                        
-                                        
+
                                         <h5 className='uiph'>Uploaded image is previewed here</h5>
                                     }
                                 </label>
                             </div>
-                        </> 
+                        </>
 
                     </div>
 
-                   
+
 
                     <div className='sectionIIc-next'>
 
-                    <Link to="/admin/sectionIII" className='sectionIIc-next-link'>
-                        <span> Next  </span>
-                        <BsArrowRight size={35} color='blue'/>
-                    </Link>
+                        <Link to="/admin/sectionIII" className='sectionIIc-next-link'>
+                            <span> Next  </span>
+                            <BsArrowRight size={35} color='blue' />
+                        </Link>
 
                     </div>
 
